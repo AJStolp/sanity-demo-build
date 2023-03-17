a69cd53b02422fd223c386cafae08a72d18d573a
Anthony Stolp <anthony.stolp@gmail.com>
2023-03-17 18:15:33 -0400

    resolved build-time type error

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,5 +1,9 @@
 import { createClient } from "next-sanity";
 
+// export const projectId: string = "qosix616";
+// export const dataset: string = "production";
+// export const apiVersion: string = "2021-10-21";
+
 export const projectId: string =
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
 export const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET || "";
--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,41 +1,41 @@
-import { SanityDocument } from "@sanity/client";
-import { GetStaticPaths, GetStaticProps } from "next";
-import { groq } from "next-sanity";
-import { client } from "../lib/sanity.client";
-import NavBar from "@/components/navbar";
+// import { SanityDocument } from "@sanity/client";
+// import { GetStaticPaths, GetStaticProps } from "next";
+// import { groq } from "next-sanity";
+// import { client } from "../lib/sanity.client";
+// import NavBar from "@/components/navbar";
 
-const query = groq`*[_type == "navigation" && slug.current == $slug][0]{
-  title,
-  link,
-}`;
+// const query = groq`*[_type == "navigation" && slug.current == $slug][0]{
+//   title,
+//   link,
+// }`;
 
-// Prepare Next.js to know which routes already exist
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = await client.fetch(
-    groq`*[_type == "navigation" && defined(slug.current)][]{
-      "params": { "slug": slug.current }
-    }`
-  );
+// // Prepare Next.js to know which routes already exist
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const paths = await client.fetch(
+//     groq`*[_type == "navigation" && defined(slug.current)][]{
+//       "params": { "slug": slug.current }
+//     }`
+//   );
 
-  return { paths, fallback: true };
-};
+//   return { paths, fallback: true };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const queryParams = { slug: params?.slug ?? `` };
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const queryParams = { slug: params?.slug ?? `` };
 
-  const navigation = await client.fetch(query, queryParams);
+//   const navigation = await client.fetch(query, queryParams);
 
-  return {
-    props: {
-      data: { navigation },
-    },
-  };
-};
+//   return {
+//     props: {
+//       data: { navigation },
+//     },
+//   };
+// };
 
-export default function Page({
-  data,
-}: {
-  data: { navigation: SanityDocument };
-}) {
-  return <NavBar data={[]} />;
-}
+// export default function Page({
+//   data,
+// }: {
+//   data: { navigation: SanityDocument };
+// }) {
+//   return <NavBar data={data} />;
+// }
