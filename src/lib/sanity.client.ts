0120aa203a88f608d767dea486896e1e7665f1c9
Anthony Stolp <anthony.stolp@gmail.com>
2023-03-17 18:07:44 -0400

    got preview working

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,8 +1,10 @@
 import { createClient } from "next-sanity";
 
-export const projectId: string = process.env.SANITY_PROJECT_ID!;
-export const dataset: string = process.env.SANITY_DATASET!;
-export const apiVersion: string = process.env.SANITY_API_VERSION!;
+export const projectId: string =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+export const dataset: string = process.env.NEXT_PUBLIC_SANITY_DATASET || "";
+export const apiVersion: string =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "";
 
 export const client = createClient({
   projectId,
--- src/lib/sanity.preview.ts
+++ src/lib/sanity.preview.ts
@@ -1,5 +1,3 @@
-// ./src/lib/sanity.preview.ts
-
 import { definePreview } from "next-sanity/preview";
 import { projectId, dataset } from "./sanity.client";
 
