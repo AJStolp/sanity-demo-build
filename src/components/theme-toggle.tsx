6668cee5cdb1a441a1e9f8b3658fe3c98b1159e7
Anthony Stolp <anthony.stolp@gmail.com>
2023-03-27 19:23:44 -0400

    adding theme & theme toggle

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+interface IThemeToggle {
+  children: React.ReactNode;
+}
+
+export default function ThemeToggle(props: IThemeToggle) {
+  return (
+    <>
+      <Image src={""} alt={""}></Image>
+    </>
+  );
+}
